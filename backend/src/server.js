618db03e94925fbc79fdea30ff8073a737183c67
lamecksingu <singulameck9619@gmail.com>
2025-01-12 15:55:10 +0300

    Modifying the entry point accordingly, and makes sure we can start the server in any location without problems

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./db');
 const models = require('./models'); // Import all models to register them with Sequelize
+const logger = require('./middleware/logger');
+const authMiddleware = require('./middleware/authMiddleware');
 
 const app = express();
+const authRoutes = require('./routes/auth'); // Authentication
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 // Routes
 const salonRoutes = require('./routes/salonRoutes'); // Example route
 app.use('/api/salons', salonRoutes);
+app.use('/auth', authRoutes); // Authentication route
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,8 +24,9 @@ app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
     try {
         // Sync the database
+
         await sequelize.sync({ alter: true }); // Use `alter: true` for adjusting tables to match models without data loss
-        console.log('Database synchronized successfully');
+	    console.log('Database synchronized successfully');
     } catch (err) {
         console.error('Error synchronizing database:', err);
     }
