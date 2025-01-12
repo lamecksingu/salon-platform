7c1f92c790cd453b76167a21d2133626071e8fab
lamecksingu <singulameck9619@gmail.com>
2025-01-12 15:54:03 +0300

    Removing loggs on the screen whenever trying to connect to the database

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Load environmental variables
+const dotenv = require('dotenv');
+const path = require('path');
+// require('dotenv').config({ path: '../.env' }); // Load environmental variables
 
-// Fetching values from environmental variables
+dotenv.config({ path: path.resolve(__dirname, '../.env') }); // load env var
+
+// Fetching values from environmental variables to initialize database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+	logging: false,
 });
 
+// connecting to the database
 const connectToDatabase = async () => {
 	try {
 		await sequelize.authenticate();
