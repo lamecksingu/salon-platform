29596a8d14674f904b34262d0b95a6439911b0cf
lamecksingu <singulameck9619@gmail.com>
2025-01-19 19:41:56 +0300

    added logic to remove the auto sync of database from models

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./db');
 const models = require('./models'); // Import all models to register them with Sequelize
-const logger = require('./middleware/logger');
-const authMiddleware = require('./middleware/authMiddleware');
+const loggerMiddleware = require('./middleware/logger');
+// const authMiddleware = require('./middleware/authMiddleware');
 
 const app = express();
 const authRoutes = require('./routes/auth'); // Authentication
@@ -11,12 +11,12 @@ const authRoutes = require('./routes/auth'); // Authentication
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(logger);
+app.use(loggerMiddleware);
 
 // Routes
 const salonRoutes = require('./routes/salonRoutes'); // Example route
 app.use('/api/salons', salonRoutes);
-app.use('/auth', authRoutes); // Authentication route
+app.use('/api/auth', authRoutes); // Authentication route
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,9 +24,10 @@ app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
     try {
         // Sync the database
-
-        await sequelize.sync({ alter: true }); // Use `alter: true` for adjusting tables to match models without data loss
+	    if (process.env.NODE_ENV === 'development') {
+		    await sequelize.sync(); // Use `alter: true` for adjusting tables to match models without data loss
 	    console.log('Database synchronized successfully');
+	    }
     } catch (err) {
         console.error('Error synchronizing database:', err);
     }
