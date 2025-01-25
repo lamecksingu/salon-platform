eacd0f6596ed6066e70bc48ba52de4ded49f16d5
lamecksingu <singulameck9619@gmail.com>
2025-01-25 19:12:04 +0300

    Added some logic to the authentication

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,9 +6,20 @@ const register = async (req, res) => {
     const { name, email, password, role } = req.body;
 
     try {
+	    // check if the user exists
+	    const existingUser = await User.findOne({ where: { email } });
+	    if (existingUser) {
+		    return res.status(400).json({ error: 'A user with this email already exists' });
+	    }
+
+	    //hassh password
         const hashedPassword = await bcrypt.hash(password, 10);
+
+	    //create the user
         const user = await User.create({ name, email, password: hashedPassword, role });
-        res.status(201).json({ message: 'User registered successfully', user });
+	    // exclude password from the response
+	    const { password: _, ...userWithoutPassword } = user.toJSON();
+        res.status(201).json({ message: 'User registered successfully', user: userWithoutPassword });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
@@ -24,7 +35,7 @@ const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user.id, role: user.role, salon_id: user.salon_id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         res.status(500).json({ error: error.message });
